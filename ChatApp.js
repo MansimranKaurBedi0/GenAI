9121d16acc5e8adff97633f850443b672674eeae
MANSIMRAN <mansimran1414@gmail.com>
2026-05-01 03:36:50 +0530

    History attached with aI

---
+++ ChatApp.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from "readline-sync";
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+const History=[];
+
+async function Chatting(userProblem){
+  History.push({
+  role: "user",
+  parts: [{ text: userProblem }]
+});
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: History,
+  });
+  console.log(response.text);
+  History.push({
+  role: "model",
+    parts: [{ text: response.text }]
+});
+}
+async function main() {
+  // const response = await ai.models.generateContent({
+  //   model: "gemini-3-flash-preview",
+  //   contents: "Explain how AI works in a few words",
+  // });
+  // console.log(response.text);
+  const userProblem=readlineSync.question("What do you want to ask? ");
+  await Chatting(userProblem);
+  main();
+}
+
+await main();
