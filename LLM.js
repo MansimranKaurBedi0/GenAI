7c802a922f0a088dc146f09708bbf1ae4ec6e4f3
MANSIMRAN <mansimran1414@gmail.com>
2026-05-01 03:15:31 +0530

    first commit

---
+++ LLM.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
